382e7a1043c8944475364df6ea1aa4198401b597
Sanyab8 <132088180+Sanyab8@users.noreply.github.com>
2026-05-02 12:08:29 -0700

    Update portfolio sections and responsive menu

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ let navbar = document.querySelector('.navbar');
 
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.onclick = () => {
+        navbar.classList.remove('active');
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+    }
+});
